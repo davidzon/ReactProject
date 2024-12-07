7e8bb4b6effc3e5c79ae7e30e9df1346a6ee0423
davidzon <davidson18ramirez@gmail.com>
2024-12-06 22:38:48 -0500

    index fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,15 +7,16 @@ import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
 
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
 }
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
 
 
 
