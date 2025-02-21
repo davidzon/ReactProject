8b3d47cfb4852cac16485a798f052afb28d720be
davidzon <davidson18ramirez@gmail.com>
2025-02-20 22:57:38 -0500

    fix2

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,10 +1,19 @@
-// backend/config/index.js
+// // backend/config/index.js
+// module.exports = {
+//     environment: process.env.NODE_ENV || 'development',
+//     port: process.env.PORT || 8000,
+//     dbFile: process.env.DB_FILE,
+//     jwtConfig: {
+//       secret: process.env.JWT_SECRET,
+//       expiresIn: process.env.JWT_EXPIRES_IN
+//     }
+//   };
 module.exports = {
-    environment: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 8000,
-    dbFile: process.env.DB_FILE,
-    jwtConfig: {
-      secret: process.env.JWT_SECRET,
-      expiresIn: process.env.JWT_EXPIRES_IN
-    }
-  };
+  environment: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 8000,
+  dbFile: process.env.DB_FILE || './db/development.sqlite', //  Ensure a default SQLite file path
+  jwtConfig: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN
+  }
+};
